6aefcb6edb8b924b74cfd67f66c919f785803dcd
Lorenz Equihua <panchocosa@gmail.com>
2025-03-19 21:10:59 -0600

    feat: add version management features and update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,28 @@
+## [1.2.0] - 19/03/2025
+
+### Added Version Management
+
+- Added new options to change the semantic version of the project
+- Added a new option to increase the android bundle version code. Useful for Android builds
+
 ## [1.1.1] - 12-03-2025
+
 ### Fixed Folder Creation Bug
+
 - Fixed a bug where the `External` and `Plugins` folder would not be created.
 
 ## [1.1.0] - 03-03-2025
+
 ### Initialize Repo
+
 - New option creates `.gitignore` and runs `git init`
 
 ## [1.0.0] - 03-03-2025
+
 ### First release
+
 - Allows the user to create a `_Project` folder, with the folders
-you would expect to use in a Unity project such as `Scripts`, `Materials`, 
-`Scriptable Objects`, etc
-- Downloads the open source packages I use the most, for example: 
-[Better singletons](https://github.com/UnityCommunity/UnitySingleton.git)
+  you would expect to use in a Unity project such as `Scripts`, `Materials`,
+  `Scriptable Objects`, etc
+- Downloads the open source packages I use the most, for example:
+  [Better singletons](https://github.com/UnityCommunity/UnitySingleton.git)
---
+++ Runtime/UpdateVersion.cs
@@ -0,0 +1,68 @@
+﻿using UnityEditor;
+            using UnityEngine;
+            
+            namespace LorenzTools {
+                /// <summary>
+                /// Provides utility methods for managing version numbers in Unity projects.
+                /// This class contains editor menu items for updating different version components
+                /// and managing bundle version codes.
+                /// </summary>
+                public static class UpdateVersion {
+                    /// <summary>
+                    /// Increments the major version number and resets minor and patch to zero.
+                    /// Example: 1.2.3 -> 2.0.0
+                    /// </summary>
+                    [MenuItem("Tools/Update Version/Update Major Version")]
+                    public static void UpdateMajorVersion() {
+                        var currentVersion = Application.version;
+                        var versionParts = currentVersion.Split('.');
+                        var major = int.Parse(versionParts[0]);
+                        major += 1;
+                        PlayerSettings.bundleVersion = $"{major}.0.0";
+                    }
+                    
+                    /// <summary>
+                    /// Increments the minor version number and resets patch to zero.
+                    /// Example: 1.2.3 -> 1.3.0
+                    /// </summary>
+                    [MenuItem("Tools/Update Version/Update Minor Version")]
+                    public static void UpdateMinorVersion() {
+                        var currentVersion = Application.version;
+                        var versionParts = currentVersion.Split('.');
+                        var minor = int.Parse(versionParts[1]);
+                        minor += 1;
+                        PlayerSettings.bundleVersion = $"{versionParts[0]}.{minor}.0";
+                    }
+                    
+                    /// <summary>
+                    /// Increments the patch version number.
+                    /// Example: 1.2.3 -> 1.2.4
+                    /// </summary>
+                    [MenuItem("Tools/Update Version/Update Patch Version")]
+                    public static void UpdatePatchVersion() {
+                        var currentVersion = Application.version;
+                        var versionParts = currentVersion.Split('.');
+                        var patch = int.Parse(versionParts[2]);
+                        patch += 1;
+                        PlayerSettings.bundleVersion = $"{versionParts[0]}.{versionParts[1]}.{patch}";
+                    }
+                    
+                    /// <summary>
+                    /// Resets the version number to 0.0.1.
+                    /// Useful when starting a new project or major version reset is needed.
+                    /// </summary>
+                    [MenuItem("Tools/Update Version/Reset Version")]
+                    public static void ResetVersion() {
+                        PlayerSettings.bundleVersion = "0.0.1";
+                    }
+            
+                    /// <summary>
+                    /// Increments the Android bundle version code.
+                    /// This is required for uploading new versions to the Google Play Store.
+                    /// </summary>
+                    [MenuItem("Tools/Update Version/Update Android Bundle Version Code")] 
+                    public static void UpdateAndroidBundleVersionCode() {
+                        PlayerSettings.Android.bundleVersionCode++;
+                    }
+                }
+            }
